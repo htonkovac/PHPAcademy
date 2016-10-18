fd7d47a2f8ece729d180d4ce8c241336b5b9446b
phpakademijadanprvi <upisibilosta@lol.com>
2016-10-18 19:37:16 +0200

    intial commit

---
+++ page-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>page-2</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Neki naslov</h1>
+        
+        <img src="http://localhost/static/slika1.jpg">
+        
+        <footer>
+            <p> date: <?php echo date('d.m.Y.') ?></p>
+        </footer>
+    </body>
+</html>
