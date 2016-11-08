0bd508dffd65d79c4ed2aa35f836bb910f4e33a3
htonkovac <htonkovac@gmail.com>
2016-11-08 19:53:37 +0100

    leson6

---
+++ Lesson6/mvc-app/app/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * example config file
+ */
+return [
+    //'some_config_key' => 'some config value',
+
+    'url'           => 'http://localhost/Lesson6/mvc-app/',
+    'mode'          => 'development',
+
+    'admin_email'   => 'admin',
+    'admin_password' => '0123',
+
+    'db_name'       => 'academy',
+    'db_user'       => 'root',
+    'db_password'   => '',
+];
